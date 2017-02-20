0c97620c45f5217989316f0019b669d4e89d244c
Patrick <pkloby@vt.edu>
2017-02-19 20:33:00 -0500

    tokenize

--- ast.cpp
+++ ast.cpp
@@ -50,13 +50,107 @@ int AST::buildAST(string expression)
 	return 0;
 }
 
-vector<string> AST::tokenize(string expression)
+// tokenize
+// inString - String to be tokenized
+//
+// 'tokenize' parses string and removes whitespace and returns vector of strings
+std::vector<std::string> tokenize(string inString)
 {
-	// TODO	return vector<string>(15);
-	vector<string> ret = {"(", "+", "(", "+", "2", "1", ")", "(" , "+", "3", "(", "+", "1", "1", ")", ")", ")"};
-	
-	return ret;
-}
+
+	std::vector<std::string> myVector;
+	size_t i = 0;
+	//tokenize
+
+	while (i < inString.length() ) //parse through expression
+	{
+		if (inString[i] == ' ') //skip over whitespace
+		{
+			++i;
+		}
+		else
+		{
+			// character is a left/right parentheses, plus, minus, mult, div, equal, 
+			if (inString[i] == '(' || inString[i] == ')' || inString[i] == '+' || inString[i] == '*' || inString[i] == '=')
+			{
+				myVector.push_back(std::string(1, inString[i]));
+				++i;
+			}
+			// < , > , <= , >=
+			else if (inString[i] == '>' || inString[i] == '<')
+			{
+				if (inString[i + 1] == '=')
+				{
+					if (inString[i == '>'])		myVector.push_back(">=");
+					else	myVector.push_back("<=");
+					i = i + 2;  //advance two characters
+				}
+				else
+				{
+					myVector.push_back(std::string(1, inString[i]));
+					++i;
+				}
+			}
+			// < , > , <= , >=
+
+			//  - - - - - - - - -
+			else if (inString[i] == '-')
+			{
+				if (inString[i+1] == ' ')	myVector.push_back(std::string(1, inString[i])); //operator not number
+				else
+				{
+					size_t x = 1;
+					size_t y = i+1;
+					//continue to parse until end of number
+					while (isdigit(inString[y]) || inString[y] == '.' || inString[y] == 'e' || inString[y] == '-')
+					{
+						++x;
+						++y;
+					}
+					std::string number = inString.substr(i, x); //remove number, possibly complex/decimal
+					myVector.push_back(number);
+					i = i + x;
+				}
+			}
+			// begin numbers
+			else if (isdigit(inString[i]))
+			{
+				size_t x = 1;
+				size_t y = i + 1;
+				//continue to parse until end of number
+				while (isdigit(inString[y]) || inString[y] == '.' || inString[y] == 'e' || inString[y] == '-')
+				{
+					++x;
+					++y;
+				}
+				std::string number = inString.substr(i, x); //substring is number, possibly complex/decimal
+				myVector.push_back(number);
+				i = i + x;
+			}
+			// end numbers
+			else if (isalpha(inString[i]))
+			{
+				size_t x = 1;
+				size_t y = i + 1;
+				while (isalpha(inString[y]) && inString[y] != inString.length())
+				{
+					++y;
+					++x;
+				}
+				std::string symbol = inString.substr(i, x); // substring is symbol
+				myVector.push_back(symbol);
+				i = i + x;
+			}
+			else
+			{
+				std::cout << "Error: Failed to tokenize at location: " << i << std::endl;
+			}
+
+		
+		}
+
+	} //while loop til end of expression
+	return myVector;
+} //end function
 
 string AST::toString()
 {
