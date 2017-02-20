e915feefcdfdf929c2cf58665c1aa5eb135cb0f0
Patrick Kloby <pkloby@vt.edu>
2017-02-19 21:19:07 -0500

    more tests

--- astTests.cpp
+++ astTests.cpp
@@ -5,6 +5,33 @@
 TEST_CASE( "BuildAST builds proper tree", "[ast]")
 {
 	AST tree = AST();
+	SECTION ("Build brah") 
+	{
 	tree.buildAST("(+ (+ 2 1)(+ 3(+ 1 1)))");
 	REQUIRE(tree.toString() == "2 1 + 3 1 1 + + +");
+	}
+
+	SECTION( "BuildAST builds proper tree2")
+	{
+		tree.buildAST("(+ (+ 1 2) (+ 3 4) )");
+		REQUIRE(tree.toString() == "1 2 + 3 4 + +");
+	}
+
+	SECTION( "BuildAST builds proper tree3")
+	{
+		tree.buildAST("(begin (define a 1) (define b 88) (if (< a b) b a) )");
+		REQUIRE(tree.toString() == "a 1 define b 88 define a b < b a if begin");
+	}
 }
+
+
+
+	SECTION( "BuildAST builds proper tree1")
+	{
+		tree.buildAST("(- ( + 3 (- 6 9 ) ) ( - 4 5 ) )");
+		REQUIRE(tree.toString() == "3 6 9 - + 4 5 - -");
+	}
+
+
+
+
